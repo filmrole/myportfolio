a105da3de1059e055dc178a745ec54642798634b
sofusbach <56107500+filmrole@users.noreply.github.com>
2020-09-21 19:41:27 +0200

    CSS_ændringer_myscript.js

--- wp-content/themes/astra-child-theme/functions.php
+++ wp-content/themes/astra-child-theme/functions.php
@@ -2,6 +2,9 @@
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
+  
+  wp_enqueue_style('fontAwesomeCdn','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+  
     $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
     $theme = wp_get_theme();
     wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
@@ -12,4 +15,11 @@ function my_theme_enqueue_styles() {
         array( $parenthandle ),
         $theme->get('Version') // this only works if you have Version in the style header
     );
+  
+     wp_deregister_script('jquery');
+    
+    wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+  
+  wp_enqueue_script( 'myscript', get_stylesheet_directory_uri() . '/myscript.js', array(), null, true);
+ 
 }
